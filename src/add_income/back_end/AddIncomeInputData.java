090a8653086277ff42f4c56c26028de0b8f26056
ernestyuen <ernest.yuen@mail.utoronto.ca>
2023-11-13 01:25:18 -0500

    Created Income input and output data

--- src/add_income/back_end/AddIncomeInputData.java
+++ src/add_income/back_end/AddIncomeInputData.java
@@ -1,5 +1,19 @@
 package add_income.back_end;
 
 public class AddIncomeInputData {
-    final private double
+    final private String incomeSource;
+    final private double amount;
+
+    public AddIncomeInputData(String incomeSource, double amount) {
+        this.incomeSource = incomeSource;
+        this.amount = amount;
+    }
+
+    public String getIncomeSource() {
+        return incomeSource;
+    }
+
+    double getAmount(){
+        return amount;
+    }
 }
