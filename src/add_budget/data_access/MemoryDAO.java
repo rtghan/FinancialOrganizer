56cb34e65f0c24f4e7e26f3b190e451f4817bc6c
ethan <chan.ethan.2018@gmail.com>
2023-12-01 01:27:11 -0500

    modified presenter so that it switches to the homescreen upon creation of budget

--- src/add_budget/data_access/MemoryDAO.java
+++ src/add_budget/data_access/MemoryDAO.java
@@ -3,13 +3,14 @@ import entity.Budget;
 import add_budget.back_end.AddBudgetDataAccessInterface;
 import java.util.HashMap;
 import java.util.Map;
+import java.time.Month;
 public class MemoryDAO implements AddBudgetDataAccessInterface{
-    private final Map<String, Budget> budgets = new HashMap<>();
+    private final Map<Month, Budget> budgets = new HashMap<>();
 
     // can use default constructor, so don't need to write a specific one
 
     @Override
-    public void save(String monthName, Budget budget){
-        budgets.put(monthName, budget);
+    public void save(Month month, Budget budget){
+        budgets.put(month, budget);
     }
 }
--- src/interface_adapter/add_budget/AddBudgetPresenter.java
+++ src/interface_adapter/add_budget/AddBudgetPresenter.java
@@ -2,21 +2,30 @@ package interface_adapter.add_budget;
 
 import add_budget.back_end.AddBudgetOutputBoundary;
 import add_budget.back_end.AddBudgetOutputData;
-import interface_adapter.add_budget.AddBudgetViewModel;
-import interface_adapter.add_budget.AddBudgetState;
+import interface_adapter.ViewManagerModel;
+
+// TODO: replace this with the visualization screen
+import interface_adapter.home_screen.HomeScreenViewModel;
 
 public class AddBudgetPresenter implements AddBudgetOutputBoundary{
-    private final AddBudgetViewModel abVM;
-    public AddBudgetPresenter(AddBudgetViewModel abVM) {
-        this.abVM = abVM;
+    private final AddBudgetViewModel addBudVM;
+    private ViewManagerModel viewManagerModel;
+    private HomeScreenViewModel homeScreenVM;
+
+    public AddBudgetPresenter(AddBudgetViewModel addBudVM, ViewManagerModel viewManagerModel, HomeScreenViewModel homeScreenVM) {
+        this.addBudVM = addBudVM;
+        this.viewManagerModel = viewManagerModel;
+        this.homeScreenVM = homeScreenVM;
     }
     @Override
     public void prepareSuccessView(AddBudgetOutputData outputData) {
-        AddBudgetState state = abVM.getState();
+        AddBudgetState state = addBudVM.getState();
         state.setCreationSuccess(true);
-        this.abVM.setState(state);
-        abVM.firePropertyChanged();
+        this.addBudVM.setState(state);
+        addBudVM.firePropertyChanged();
 
-        // TODO: on success, switch to the view stats/homescreen panel
+        // TODO: on success, switch to the view stats panel
+        this.viewManagerModel.setActiveView(homeScreenVM.getViewName());
+        this.viewManagerModel.firePropertyChanged();
     }
 }
