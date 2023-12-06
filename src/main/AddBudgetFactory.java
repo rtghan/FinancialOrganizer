4d2c54b5416dde8a9f656472c05d26f2b57655cd
ernestyuen <ernest.yuen@mail.utoronto.ca>
2023-12-06 00:33:13 -0500

    Cleanup

--- src/main/AddBudgetFactory.java
+++ src/main/AddBudgetFactory.java
@@ -6,7 +6,7 @@ import back_end.add_budget.AddBudgetInteractor;
 import back_end.add_budget.AddBudgetOutputBoundary;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.add_budget.*;
-import back_end.add_budget.back_end.*;
+import back_end.add_budget.*;
 // TODO: replace homescreen with stats screen
 import interface_adapter.home_screen.HomeScreenViewModel;
 import view.AddBudgetView;
