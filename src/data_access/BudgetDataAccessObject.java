936395a3308f6d5675b47a45c92b07019b6db427
Veronica Smith <veronica.smith@mail.utoronto.ca>
2023-12-06 02:52:13 -0500

    docstrings for data_access classes

--- src/data_access/BudgetDataAccessObject.java
+++ src/data_access/BudgetDataAccessObject.java
@@ -6,13 +6,24 @@ import java.util.HashMap;
 import java.util.Map;
 import java.time.Month;
 
+/**
+ * implements BudgetDataAccessInterface and accesses Budget data
+ */
 public class BudgetDataAccessObject implements BudgetDataAccessInterface {
-
     private final Map<Month, Budget> budgetMap;
+
+    /**
+     * constructs Budget DAO with a hashmap
+     */
     public BudgetDataAccessObject() {
         this.budgetMap = new HashMap<>();
     }
 
+    /**
+     * gets the budget by the specified month
+     * @param month     month
+     * @return          budget for month
+     */
     @Override
     public Budget getBudgetByMonth(Month month) {
         //System.out.println(budgetMap.get(month));
--- src/data_access/ExpenseDataAccessObject.java
+++ src/data_access/ExpenseDataAccessObject.java
@@ -1,18 +1,29 @@
 package data_access;
 
 import entity.Expense;
-
 import java.time.Month;
 import java.util.Map;
 
+/**
+ * implements ExpenseDataAccessInterface and accesses Expense data
+ */
 public class ExpenseDataAccessObject implements ExpenseDataAccessInterface {
 
     private final Map<Month, Expense> expenseMap;
 
+    /**
+     * constructs Expense DAO with a hashmap
+     * @param expenseMap
+     */
     public ExpenseDataAccessObject(Map<Month, Expense> expenseMap) {
         this.expenseMap = expenseMap;
     }
 
+    /**
+     * gets the Expense by the specified month
+     * @param month     month
+     * @return          Expense for month
+     */
     @Override
     public Expense getExpensesByMonth(Month month) {
         return expenseMap.get(month);
