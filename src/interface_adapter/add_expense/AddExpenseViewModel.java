f57fe860c79ad2dce85232a83dc46b8d98432ce0
Gr4ffyGw3n <98187030+Gr4ffyGw3n@users.noreply.github.com>
2023-12-06 19:44:16 -0500

    small UI text change

--- src/interface_adapter/add_expense/AddExpenseViewModel.java
+++ src/interface_adapter/add_expense/AddExpenseViewModel.java
@@ -13,7 +13,7 @@ public class AddExpenseViewModel extends ViewModel {
     public static final String CATEGORY_TEXT_LABEL = "Category";
     public static final String AMT_TEXT_LABEL = "Amount";
     public static final String  CANCEL_BUTTON_LABEL = "Cancel";
-    public static final String MONTH_LABEL = "Select Month";
+    public static final String MONTH_LABEL = "Select Month of Expense: ";
 
     public static final String[] MONTH_COMBO = {"Current Month", "January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "December"};
